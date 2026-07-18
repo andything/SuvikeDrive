8e7bd32e7ad3ef726942b784d9cfab9fec3adae9
andything <andything@qq.com>
2026-07-19 01:37:16 +0800

    Initial Commit

---
+++ SuvikeDrive/SuvikeDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuvikeDriveApp.swift
+//  SuvikeDrive
+//
+//  Created by Andything on 2026/7/19.
+//
+
+import SwiftUI
+
+@main
+struct SuvikeDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
